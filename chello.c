759480ea7d8c1846a226c4bca2bdd1cced27eb9f
mac <macrossluosa@icloud.com>
2015-12-15 21:31:12 +0800

    linuxC leaning

---
+++ chello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+char *c;
+	c="say hello";
+printf("%s",c);
+return 0 ;
+}
---
+++ size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("int%d\n",sizeof(int)); 
+	printf("lang%d\n",sizeof(long)); 
+	printf("double%d\n",sizeof(double)); 
+	printf("float%d\n",sizeof(float)); 
+	printf("char%d\n",sizeof(char)); 
+}
+
